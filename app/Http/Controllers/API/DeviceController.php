d83a5c37de00496debb413939ac272226a174820
bagus-Arya <bagusarya850@gmail.com>
2024-11-26 07:32:54 +0000

    update:fuction

--- app/Http/Controllers/API/DeviceController.php
+++ app/Http/Controllers/API/DeviceController.php
@@ -19,13 +19,113 @@ class DeviceController extends Controller
         return $logs;
     }
 
-    public function predictOneWeek(){
-        $data = DeviceLogs::orderBy('created_at', 'DESC') -> paginate(10);
-        return $data;
+    public function predictOneWeek()
+    {
+        $data = DeviceLogs::orderBy('created_at', 'DESC')->take(10)->get();
+    
+        $suhuSum = 0;
+        $kecepatanAnginSum = 0;
+        $tekananUdaraSum = 0;
+        $kelembabanSum = 0;
+        $count = count($data);
+    
+        foreach ($data as $log) {
+            $suhuSum += $log->suhu;
+            $kecepatanAnginSum += $log->kecepatan_angin;
+            $tekananUdaraSum += $log->tekanan_udara;
+            $kelembabanSum += $log->kelembaban;
+        }
+    
+        $averageSuhu = $count > 0 ? $suhuSum / $count : 0;
+        $averageKecepatanAngin = $count > 0 ? $kecepatanAnginSum / $count : 0;
+        $averageTekananUdara = $count > 0 ? $tekananUdaraSum / $count : 0;
+        $averageKelembaban = $count > 0 ? $kelembabanSum / $count : 0;
+    
+        $predictions = [];
+        for ($i = 0; $i <= 7; $i++) {
+            $date = now()->addDays($i)->toDateString();
+            $predictions[] = [
+                'date' => $date,
+                'predicted_suhu' => $averageSuhu,
+                'predicted_kecepatan_angin' => $averageKecepatanAngin,
+                'predicted_tekanan_udara' => $averageTekananUdara,
+                'predicted_kelembaban' => $averageKelembaban,
+            ];
+        }
+    
+        return response()->json(['data' => $predictions]);
     }
 
-    public function rainHistory(){
-        // 
+    public function evaluateWeatherConditions()
+    {
+        $data = DeviceLogs::orderBy('created_at', 'DESC')->get();
+
+        $weatherConditions = [];
+        $dailyData = [];
+
+        foreach ($data as $log) {
+    
+            $date = \Carbon\Carbon::parse($log->created_at)->toDateString();
+
+            if (!isset($dailyData[$date])) {
+                $dailyData[$date] = [
+                    'total_suhu' => 0,
+                    'total_kelembaban' => 0,
+                    'total_kecepatan_angin' => 0,
+                    'count' => 0,
+                ];
+            }
+
+            $dailyData[$date]['total_suhu'] += $log->suhu;
+            $dailyData[$date]['total_kelembaban'] += $log->kelembaban;
+            $dailyData[$date]['total_kecepatan_angin'] += $log->kecepatan_angin;
+            $dailyData[$date]['count']++;
+
+            if ($log->suhu < 23 && $log->kelembaban > 1013 && $log->kecepatan_angin > 10) {
+                $condition = 'Kondisi Cuaca Buruk'; 
+            } elseif ($log->suhu > 26 && $log->kelembaban < 93 && $log->kecepatan_angin < 10) {
+                $condition = 'Kondisi Cuaca Baik'; 
+            } else {
+                $condition = 'Kondisi Tidak Diketahui'; 
+            }
+
+            $weatherConditions[] = [
+                'id' => $log->id,
+                'machine_id' => $log->machine_id,
+                'suhu' => $log->suhu,
+                'kelembaban' => $log->kelembaban,
+                'kecepatan_angin' => $log->kecepatan_angin,
+                'condition' => $condition,
+                'created_at' => $log->created_at,
+            ];
+    }
+
+    $dailyAverages = [];
+    foreach ($dailyData as $date => $values) {
+        $dailyAverages[$date] = [
+            'average_suhu' => $values['total_suhu'] / $values['count'],
+            'average_kelembaban' => $values['total_kelembaban'] / $values['count'],
+            'average_kecepatan_angin' => $values['total_kecepatan_angin'] / $values['count'],
+        ];
+    }
+
+    return response()->json([
+        'weather_conditions' => $weatherConditions,
+        'daily_averages' => $dailyAverages,
+    ]);
+}
+
+    public function showRainyConditions()
+    {
+
+        $data = DeviceLogs::orderBy('created_at', 'DESC')->get();
+    
+
+        $rainyConditions = $data->filter(function ($log) {
+            return $log->kelembaban > 80; 
+        });
+    
+        return response()->json(['rainy_conditions' => $rainyConditions]);
     }
 
     public function createLogs(Request $request) {
